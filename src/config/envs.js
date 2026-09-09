18a56cdbf3650a14ef6479983b54ebf31be62028
KevinPrieto <kevinandresprietomora@gmail.com>
2026-09-09 12:25:55 -0500

    "Arreglar conexión y README.md"

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('Falta la variable de entorno DATABASE_URL');
+}
+
 module.exports = {
   port: process.env.PORT,
   databaseUrl: process.env.DATABASE_URL,
