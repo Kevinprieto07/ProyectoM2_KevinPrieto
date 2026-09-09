3b4f6e411931dd75b2b5a9e15a5d8f9a0e090423
KevinPrieto <kevinandresprietomora@gmail.com>
2026-09-09 12:10:17 -0500

    "Conexión con Railway y creación del README.md"

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
 const { databaseUrl } = require('./envs');
 
+const isLocal = databaseUrl.includes('localhost') || databaseUrl.includes('127.0.0.1');
+
 const pool = new Pool({
   connectionString: databaseUrl,
+  ssl: isLocal ? false : { rejectUnauthorized: false },
 });
 
 module.exports = pool;
